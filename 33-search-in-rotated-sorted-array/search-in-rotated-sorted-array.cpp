3ac5750b0d36128923451ca9c635c2be340ec099
Yashodip Waghmode <yashodipwaghmode2425@gmail.com>
2026-08-23 10:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (36.94%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-
-       int st=0;
-       int end=nums.size()-1;
-       while(st<=end)
-       {
-        int mid;
-        mid=(st+end)/2;
-        if(nums[mid]==target)
+    int search(vector<int>& nums, int k) {
+        int n=nums.size();
+        int st=0;
+        int end=n-1;
+        while (st<=end)
         {
-            return mid;
+            int mid=(st+end)/2;
+            if(nums[mid]==k)
+            {
+                return mid;
+            }
+            if(nums[st]<=nums[mid]) //left is sorted
+            {
+                if(nums[st]<=k && k<=nums[mid])// target is in right side
+                {
+                    end=mid-1;
+                }
+                else // target is in right side
+                {
+                    st=mid+1;
+                }
+            }
+            else //right is sorted
+            {
+                if(nums[mid]<=k && k<=nums[end])  // target is in right side
+                {
+                    st=mid+1;
 
-        }
-        if(nums[st]<=nums[mid]) //left part is sorted
-        {
-            if(nums[st]<=target && target<=nums[mid])// search in laft part
-            {
-                end=mid-1;
+                }
+                else
+                {
+                    end=mid-1;  // target is in right side
+                }
 
             }
-            else //search in right part
-            {
-                st=mid+1;
-            }
         }
-        else //right part is sorted
-        {
-            if(nums[mid]<=target && target<=nums[end])//search in right part
-            {
-                st=mid+1;
-            }
-            else //search in left part
-            {
-                end=mid-1;
-            }
-
-        }
-       }
-       return -1;
+        return -1;
+       
+        
     }
 };
